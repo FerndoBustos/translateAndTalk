d6edbc54408781d8d00675ef6f7dd1041adf5038
Bustos Ramírez Luis Fernando <desarrollo16@iidinf0j076333.local>
2016-09-01 11:00:51 -0500

    Initial Commit

---
+++ readItTests/readItTests.swift
@@ -0,0 +1,36 @@
+//
+//  readItTests.swift
+//  readItTests
+//
+//  Created by Bustos Ramírez Luis Fernando on 01/09/16.
+//  Copyright © 2016 Bustos Ramírez Luis Fernando. All rights reserved.
+//
+
+import XCTest
+@testable import readIt
+
+class readItTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
